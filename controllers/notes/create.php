39f136395de89960f6f1ad16b4b6b0f800d53db9
Liam Corbett <156666208+Liam-Corbett-Tagmarshal@users.noreply.github.com>
2025-03-02 15:54:49 +0200

    Refactor routing system: implement Router class, update route handling, and add HTTP method support

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -3,8 +3,6 @@
 use Core\Database;
 use Core\Validator;
 
-require base_path("Validator.php");
-
 $config = require base_path("config.php");
 
 $db = new Database($config["database"]);
@@ -15,11 +13,9 @@ $errors = [];
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-
     if (!Validator::string($_POST["body"], 1, 1000)) {
         $errors["body"] = "A body of no more than 1000 characters is required.";
     }
-
 
     if (empty($errors)) {
         $db->query(("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)"), [
