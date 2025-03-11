a0e61398401f9fc2fad3b9383018659492f4eaff
Liam Corbett <156666208+Liam-Corbett-Tagmarshal@users.noreply.github.com>
2025-03-11 18:26:30 +0200

    Refactor notes functionality: implement note creation and deletion, update routing, and improve error handling

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -11,14 +11,15 @@ $currentUserId = 1;
 // $id = $_GET["id"];
 
 $note = $db->query("SELECT * FROM notes where id = :id", [
-    "id" => $_GET["id"]
+    "id" => $_POST["id"]
 ])->findOrFail();
 
 authorise($note["user_id"] === $currentUserId);
 
-
-view("notes/show.view.php", [
-    "heading" => "Note",
-    "note" => $note
+$db->query("DELETE FROM notes WHERE id = :id", [
+    "id" => $_POST["id"]
 ]);
+
+header("location: /notes");
+exit();
 
--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ $router = new \Core\Router;
 $routes = require base_path("routes.php");
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 
-$method = $POST["_method"] ?? $_SERVER["REQUEST_METHOD"];
+$method = $_POST["_method"] ?? $_SERVER["REQUEST_METHOD"];
 
 $router->route($uri, $method);
 
