8d132ad2cf7c3fee9e3155092d3cdec63e44a90e
Liam Corbett <156666208+Liam-Corbett-Tagmarshal@users.noreply.github.com>
2025-06-16 19:58:20 +0200

    Add validation exception handling and refactor login form validation logic

--- Core/router.php
+++ Core/router.php
@@ -77,6 +77,11 @@ class Router {
         $this->abort();
     }
 
+    public function previousUrl()
+    {
+        return $_SERVER['HTTP_REFERER'];
+    }
+
     function abort($code = 404) {
 
         http_response_code($code);
--- public/index.php
+++ public/index.php
@@ -1,30 +1,35 @@
 <?php
 
 use Core\Session;
+use Core\ValidationException;
 
 session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 
-require BASE_PATH . "Core/functions.php";
+require BASE_PATH . 'Core/functions.php';
 
 spl_autoload_register(function ($class) {
-    //Core\Database
-    $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
     require base_path("{$class}.php");
 });
 
-require base_path(("bootstrap.php"));
+require base_path('bootstrap.php');
 
+$router = new \Core\Router();
+$routes = require base_path('routes.php');
 
-$router = new \Core\Router;
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-$routes = require base_path("routes.php");
-$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+try {
+    $router->route($uri, $method);
+} catch (ValidationException $exception) {
+    Session::flash('errors', $exception->errors);
+    Session::flash('old', $exception->old);
 
-$method = $_POST["_method"] ?? $_SERVER["REQUEST_METHOD"];
-
-$router->route($uri, $method);
+    return redirect($router->previousUrl());
+}
 
 Session::unflash();
